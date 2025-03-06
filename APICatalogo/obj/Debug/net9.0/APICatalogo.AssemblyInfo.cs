8ef74e125115fc023da6db5ea3fc0253137e4899
PabloQuadros <pablo.aquadros@gmail.com>
2025-03-06 02:05:55 -0300

    ExceptionHandler Test

--- APICatalogo/obj/Debug/net9.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net9.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b235f00116a37f13beea54598d664cf676ddc7de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8651c2cc256250fb585d9c17c875db73f279d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
