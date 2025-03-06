0a8651c2cc256250fb585d9c17c875db73f279d3
PabloQuadros <pablo.aquadros@gmail.com>
2025-03-06 01:29:53 -0300

    custom attribute validation test

--- APICatalogo/Models/Category.cs
+++ APICatalogo/Models/Category.cs
@@ -1,10 +1,27 @@
-﻿namespace APICatalogo.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Validations;
+using System.ComponentModel.DataAnnotations;
 
-public class Category
+namespace APICatalogo.Models;
+
+public class Category : IValidatableObject
 {
     public int CategoryId { get; set; }
     public string? Name { get; set; }
     public string? ImageUrl { get; set; }
     public ICollection<Product> Products { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(IValidationContext validationConterxt)
+    {
+        if(!string.IsNullOrEmpty(this.Name))
+        {
+            var firstLetter = this.Name.ToString()[0].ToString();
+
+            if (firstLetter != firstLetter.ToUpper())
+            {
+                yield return new ValidationResult("The first letter at product name must be capitalized", new[] { nameof(this.Name) });
+            }
+        }
+    }
 
 }
